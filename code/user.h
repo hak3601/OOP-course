9b94db08f59fc93b45039d8533644e920a84775e
moonwalk <moonwalk725@gmail.com>
2024-11-13 15:14:28 +0900

    merging prof.h, stud.h into user.h

--- code/user.h
+++ code/user.h
@@ -13,9 +13,38 @@ public:
     User(int );
     User(const User&);
     ~User();
+};
 
+class Student : public User{
+private:
+    vector<string> enroled_courses;
 
+public:
+    Student(string, int);
+    Student(const Student&);
+    ~Student();
 
+    Student& operator=(const Student&);
+
+    friend void displayEnroledCourses();
+    friend void take_exam();
+};
+
+class Professor : public User{
+private:
+    vector<string> instructing_courses;
+
+public:
+    Professor(string, int);
+    Professor(const Professor&);
+    ~Professor();
+
+    Professor& operator=(const Professor&);
+
+    friend void displayInstructingCourses();
+    friend void create_exam();
+    friend void mark_exam();
+    friend void comment_exam();
 };
 
 #endif 
