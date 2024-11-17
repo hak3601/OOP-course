7517090d7e7b20dadc50d67a62c076520b76f819
kimsumin <crapy0001@cau.ac.kr>
2024-11-17 16:18:24 +0900

    update DDE and PT
    Because they need to be clear about what to do.

--- code/dynamic_difficulty_engine.cpp
+++ code/dynamic_difficulty_engine.cpp
@@ -10,32 +10,19 @@ private:
     float current_student_correct_rate;  // tracing student's performance(I think it would be from performance tracker)
     unsigned int current_problem_number; // current question's number
     int current_difficulty_level;  // current difficulty level
-    vector<int> question_set;
 
 public:
     // initialize difficulty engine
     DynamicDifficultyEngine(int initial_difficulty) : current_difficulty_level(initial_difficulty), current_problem_number(0), current_student_correct_rate(0.0f) {
-        // generate the questions with various difficulty from 1 to 10.
-        for (int i = 1; i <= 10; ++i) {
-            question_set.push_back(i);
-        }
-        srand(time(nullptr));  // initialize random value
+        
     }
 
     // get new question from the question set 
     int getNextQuestion(bool correct_answer) {
         adjustDifficulty(correct_answer);  // adjusting difficulty of the new question
 
-        // bring the question closest difficulty
-        int closest_difficulty = question_set[0];
-        for (int question_difficulty : question_set) {
-            if (abs(question_difficulty - current_difficulty_level) < abs(closest_difficulty - current_difficulty_level)) {
-                closest_difficulty = question_difficulty;
-            }
-        }
-
-        cout << "Next question difficulty: " << closest_difficulty << endl;
-        return closest_difficulty;
+        cout << "Next question difficulty: " << endl;
+        // return proper_difficulty (may be problem set);
     }
 
     // help getNextQuestion to get the proper question reflecting the difficulty
